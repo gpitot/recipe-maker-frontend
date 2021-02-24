4e648784e25bb8018c15da1c1f2cf137e89cc267
Guillaume Pitot <gpitot@atlassian.com>
2021-02-24 16:44:27 +1100

    remove cookie after storing

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -2,10 +2,9 @@ const getCookie = () => {
   const url = new URL(window.location.href);
   const params = new URLSearchParams(url.search);
   const cookie = params.get("cookie");
-
-  console.log("[g] cookie is ", cookie);
   if (cookie) {
     window.localStorage.setItem("auth-cookie", cookie);
+    window.history.replaceState(null, document.title, window.location.pathname);
   }
 };
 
