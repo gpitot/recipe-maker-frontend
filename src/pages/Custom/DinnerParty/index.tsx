fb748fab4bbd8c30506252d9b987094889562457
Guillaume Pitot <gpitot@atlassian.com>
2021-03-26 11:02:21 +1100

    dinner party message

--- src/pages/Custom/DinnerParty/index.tsx
+++ src/pages/Custom/DinnerParty/index.tsx
@@ -5,12 +5,16 @@ import Button from "components/Button";
 const DinnerParty = () => (
   <Information>
     <h2>Court 1 dinner party!</h2>
-    <p>Support the club with by joining a fantastic dinner event.</p>
-    <p>Celebrity chef ***** hosting a delicious meal.</p>
-    <p>Text goes here.....</p>
-    <p>All profits go to replacing the lights.</p>
-    <p>Text goes here.....</p>
-    <Button text="Join in!" href="https://checkout.square.site/buy/NVCN23MCKFBUGAVEG2TV6HHJ" type="link" />
+    <p>Saturday 1st May, 6.30pm</p>
+    <p>Support the club by joining a fantastic dinner event.</p>
+    <p>Catering by Sydney Cooking School</p>
+    <p>All profits go towards replacing the court lights.</p>
+    <p>Any enquiries please contact Michelle on 0414 307 547</p>
+    <Button
+      text="Sign up here"
+      href="https://checkout.square.site/buy/NVCN23MCKFBUGAVEG2TV6HHJ"
+      type="link"
+    />
   </Information>
 );
 
