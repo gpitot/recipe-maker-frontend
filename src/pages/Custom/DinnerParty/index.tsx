3cf65400c5f0f7a6f3414a55622adbb4bbdafd59
Guillaume Pitot <gpitot@atlassian.com>
2021-03-29 15:45:46 +1100

    text update

--- src/pages/Custom/DinnerParty/index.tsx
+++ src/pages/Custom/DinnerParty/index.tsx
@@ -6,7 +6,7 @@ const DinnerParty = () => (
   <Information>
     <h2>Court 1 dinner party!</h2>
     <p>Saturday 1st May, 6.30pm</p>
-    <p>Support the club by joining a fantastic dinner event.</p>
+    <p>Support the club by joining a fantastic dinner event with 2 courses.</p>
     <p>Catering by Sydney Cooking School</p>
     <p>All profits go towards replacing the court lights.</p>
     <p>Any enquiries please contact Michelle on 0414 307 547</p>
