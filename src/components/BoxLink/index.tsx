3ef62e363ce7fb3205f4c4fea1236651628c41ba
Guillaume <gpitot@atlassian.com>
2021-05-03 14:46:25 +1000

    boxlink is fully linked

--- src/components/BoxLink/index.tsx
+++ src/components/BoxLink/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import style from "./style.module.scss";
 import Information from "components/Information";
 import Button from "components/Button";
-import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 interface IProps {
   id: number;
@@ -13,17 +13,18 @@ interface IProps {
 
 const BoxLink = (details: IProps) => {
   const { description, id, name, link } = details;
-  const history = useHistory();
-  const handlePlay = () => {
-    history.push(`${link}/${id}`, details);
-  };
 
   return (
-    <Information styles={style["table-outer"]}>
-      <h3>{name}</h3>
-      <h5>{description}</h5>
-      <Button text={"Play now"} handleClick={handlePlay} />
-    </Information>
+    <Link
+      to={{ pathname: `${link}/${id}`, state: details }}
+      className={style["no-link"]}
+    >
+      <Information styles={style["table-outer"]}>
+        <h3>{name}</h3>
+        <h5>{description}</h5>
+        <Button text={"Play now"} />
+      </Information>
+    </Link>
   );
 };
 
