665e645c7800702afda28730519bf1c6fc833b76
Guillaume Pitot <gpitot@atlassian.com>
2021-06-02 13:40:52 +1000

    added better info for streaks

--- src/components/Ads/streak-incentive.tsx
+++ src/components/Ads/streak-incentive.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import Information from "components/Information";
 import style from "./style.module.scss";
 import classnames from "classnames";
+import Streak from "components/Streak";
+
 const StreakIncentive = () => (
   <div className={style.streak}>
-    <Information styles={classnames(style.gap)}>
-      <h3>Play 3 weeks in a row and get the 4th free!</h3>
+    <Information styles={classnames(style.gap, style["no-underline"])}>
+      <h3 className={style["streak-info"]}>
+        Play <Streak streak={3} />
+      </h3>
+      <h3 className={style["streak-heading"]}>
+        weeks in a row and get the 4th free!
+      </h3>
     </Information>
   </div>
 );
