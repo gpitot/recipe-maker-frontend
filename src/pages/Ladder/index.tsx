3badf2b6a1288fcaea98e9ecc68d116a5e017e71
Guillaume Pitot <gpitot@atlassian.com>
2021-06-15 11:58:29 +1000

    recent results first

--- src/pages/Ladder/index.tsx
+++ src/pages/Ladder/index.tsx
@@ -22,8 +22,8 @@ const Ladder = () => {
     <>
       <LadderLeagueAd />
       <Ranks ladderid={id} />
-      <Matches ladderid={id} challenges={true} />
       <Matches ladderid={id} challenges={false} />
+      <Matches ladderid={id} challenges={true} />
     </>
   );
 };
