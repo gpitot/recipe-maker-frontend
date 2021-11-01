b3b5768060d79129b707e9c744b6246878376303
gpitot <gpitot@atlassian.com>
2021-11-01 17:20:57 +1100

    redirect comp - to ladder league #1

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
 import Base from "components/Base";
 import Home from "pages/Home";
 import Social from "pages/Social";
-import Competition from "pages/Competition";
 import Ladder from "pages/Ladder";
 import Event from "pages/Event";
 
@@ -31,7 +30,7 @@ const Routes = () => (
           <Event />
         </Route>
         <Route exact path="/competition">
-          <Competition />
+          <Redirect to={"/competition/ladder/1"}/>
         </Route>
 
         <Route path="/competition/ladder/:ladderid">
