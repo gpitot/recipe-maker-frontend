a94330c91058580906c5ec95554b0932d134d981
gpitot <gpitot@atlassian.com>
2021-11-06 14:40:09 +1100

    admin api

---
+++ src/rest/admin/index.ts
@@ -0,0 +1,47 @@
+import axios from "axios";
+import {
+    IJsonResponse,
+    BASE_URL,
+    commonAxiosConfig,
+    IResultResponse,
+} from "rest/common";
+import {IUser} from "../users";
+
+
+interface ISocialSignups extends Pick<IUser, "id"| "firstname"| "lastname"> {
+    count : number;
+}
+
+enum MessageKeys {
+    "social-freebie-reminder" = "social-freebie-reminder",
+    "social-recent-user" = "social-recent-user"
+}
+
+const api = {
+    getSocialSignups: (order : "ASC" | "DESC", limit? : number) => {
+        return axios
+            .get<null, IJsonResponse<ISocialSignups>>(`${BASE_URL}/admin/social-signups?order=${order}&limit=${limit}`)
+            .then((res) => {
+                return res.data;
+            });
+    },
+
+    getPotentialSocials: (mode : "RECENT" | "FREEBIE") => {
+        return axios
+            .get<null, IJsonResponse<Pick<IUser, "id"| "firstname"| "lastname"| "phone">>>(`${BASE_URL}/admin/potential-socials`)
+            .then((res) => {
+                return res.data;
+            });
+    },
+
+    sendGroupMessage: (users : IUser[], messageKey : MessageKeys) => {
+        return axios
+            .post<null, IJsonResponse<IResultResponse>>(`${BASE_URL}/admin/send-group-message`, {
+                users,
+                messageKey
+            }, commonAxiosConfig)
+            .then((res) => {
+                return res.data;
+            });
+    },
+}
